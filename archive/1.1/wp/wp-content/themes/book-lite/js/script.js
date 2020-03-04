99880255c139c7ddf716b9d9c6fa329c95fee754
Asia Hoe <asia.hoe@gmail.com>
2020-03-04 18:09:58 -0500

    Re-initalize the repository.

--- archive/1.1/wp/wp-content/themes/book-lite/js/script.js
+++
@@ -1,9 +0,0 @@
-jQuery( document ).ready( function( $ ) {
-	$( '.gallery-icon a' ).hover(
-		function() {
-    		$(this).parent().parent().find( '.gallery-caption' ).fadeIn(200);
-		}, 
-		function() {
-			$(this).parent().parent().find( '.gallery-caption' ).fadeOut(200);
-		} )
-} );
